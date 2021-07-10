a02d43190a4b38922dcdb38b77d222a071696373
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-07-10 13:43:47 +0400

    Created the orgs login endpoint

---
+++ src/routes/orgsLogin.py
@@ -0,0 +1,55 @@
+import json
+
+import flask
+from bson import json_util
+
+orgsLoginBP = flask.Blueprint("orgsLogin", __name__)
+
+from main import orgsCol
+
+
+@orgsLoginBP.route("/api/v1/orgs/login", methods=["GET", "POST"])
+async def orgsSignupFunction():
+    data = flask.request.get_json(force=True)
+    if data.get("username") and data.get("password"):
+        account = {
+            "username": data.get("username"),
+            "password": data.get("password"),
+        }
+        account_match = orgsCol.find(account).limit(1)
+        if account_match.count() == 0:
+            return (
+                flask.jsonify(
+                    {
+                        "code": 401,
+                        "content": None,
+                        "message": "Incorrect username or password.",
+                        "success": False,
+                    }
+                ),
+                401,
+            )
+        else:
+            return (
+                flask.jsonify(
+                    {
+                        "code": 200,
+                        "content": json.loads(json_util.dumps(account_match[0])),
+                        "message": "Account credentials found successfully.",
+                        "success": True,
+                    }
+                ),
+                200,
+            )
+    else:
+        return (
+            flask.jsonify(
+                {
+                    "code": 422,
+                    "content": None,
+                    "message": "Malformed data provided.",
+                    "success": False,
+                }
+            ),
+            422,
+        )
